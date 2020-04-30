7044e3e5183a2837c31e309dfee86c419bac8407
Domryuken <ladonjackson@hotmail.co.uk>
2020-04-30 20:45:15 +0100

    working

--- searchMetaData.py
+++ searchMetaData.py
@@ -13,50 +13,17 @@ pageDescs = []
 searchTerm = input("Enter search term: ")
 print("...")
 
-url = "https://www.google.com/search?start=1&q=" + searchTerm.replace(" ", "+")
-fileName = searchTerm.replace(" ", "_") + "_results"
-
-
-
-def printOut():
-    for i in range(len(resultDescs)):
-        print("Result title: " + resultTitles[i])
-        print("Page title: " + pageTitles[i])
-        print("Result meta Description: " + resultDescs[i])
-        print("Page meta Description: " + pageDescs[i])
-        print("Link: " + links[i])
-        print("=====================")
-
-
-
-def writeToJson():
-    file = open(fileName + ".json","w+")
-    file.write('{"Search term":"' + searchTerm + '","Results":[')
-
-    for i in range(len(resultDescs)):
-        if i > 0:
-            file.write(',')
-        resultTitle = '"Result title":' + json.dumps(resultTitles[i])
-        metaTitle = '"Page title":' + json.dumps(pageTitles[i])
-        resultDescription = '"Result meta description":' + json.dumps(resultDescs[i])
-        metaDescription = '"Actual meta description":' + json.dumps(pageDescs[i])
-        link = '"Link":' + json.dumps(links[i])
-
-        file.write("{" + resultTitle + "," + metaTitle + "," + resultDescription + "," + metaDescription + "," + link + "}")
-
-    file.write(']}')
-    file.close()
-
+url = f"https://www.google.com/search?q={searchTerm.replace(' ', '+')}"
+fileName = f"{searchTerm.replace(' ', '_')}_results"
 
 def writeToCsv():
 
-    file = open(searchTerm.replace(" ", "_") + "_results.csv","w+")
+    file = open(f"{fileName}.csv", "w+", encoding="utf-8")
     writer = csv.writer(file)
 
-    writer.writerow([searchTerm, "Result meta description", "Page title", "Page meta description", "link"])
+    writer.writerow([f'=HYPERLINK("{url}","Search results for: {searchTerm}")', "Result meta description", "Page title", "Page meta description"])
     for i in range(len(resultDescs)):
-        writer.writerow([resultTitles[i], resultDescs[i], pageTitles[i], pageDescs[i], links[i]])
-    
+        writer.writerow([f'=HYPERLINK("{links[i]}","{resultTitles[i]}")', resultDescs[i], pageTitles[i], pageDescs[i]])
 
 def getPage(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -91,6 +58,7 @@ for result in results:
         pageDescs.append(pageDesc.get("content"))
 
 writeToCsv()
+
 
 print("Saved as " + fileName)
 
