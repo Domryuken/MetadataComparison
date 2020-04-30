cd1e85704077160b28f24882aa2e6f55c7053926
Domryuken <ladonjackson@hotmail.co.uk>
2020-04-30 22:36:15 +0100

    finished?

--- searchMetaData.py
+++ searchMetaData.py
@@ -14,16 +14,17 @@ searchTerm = input("Enter search term: ")
 print("...")
 
 url = f"https://www.google.com/search?q={searchTerm.replace(' ', '+')}"
-fileName = f"{searchTerm.replace(' ', '_')}_results"
+fileName = f"{searchTerm.replace(' ', '_')}_results.csv"
 
 def writeToCsv():
 
-    file = open(f"{fileName}.csv", "w+", encoding="utf-8")
+    print(f"writing to file {fileName}")
+    file = open(fileName, "w+", encoding="utf-8", newline='')
     writer = csv.writer(file)
 
-    writer.writerow([f'=HYPERLINK("{url}","Search results for: {searchTerm}")', "Result meta description", "Page title", "Page meta description"])
+    writer.writerow(["Ranking position", f'=HYPERLINK("{url}","Search results for: {searchTerm}")', "Implemented title", "Googles title", "Implemented meta description", "Googles meta description"])
     for i in range(len(resultDescs)):
-        writer.writerow([f'=HYPERLINK("{links[i]}","{resultTitles[i]}")', resultDescs[i], pageTitles[i], pageDescs[i]])
+        writer.writerow([i+1, links[i], pageTitles[i], resultTitles[i], pageDescs[i], resultDescs[i]])
 
 def getPage(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -32,35 +33,38 @@ def getPage(url):
 
 
 
-
-resultsPage = getPage(url)
-results = resultsPage("div", class_="rc")
-
-for result in results:
+def resultsForPage(number):
         
-    link = result.find("a").get("href")
-    links.append(link)
-    resultTitles.append(result.find("h3").getText())
-    resultDescs.append(result.find("span", class_="st").getText())
+    resultsPage = getPage(f"{url}&start={number}")
+    results = resultsPage("div", class_="rc")
 
-    actualPage = getPage(link)
-    
-    pageTitle = actualPage.find("title").getText()
-    if pageTitle is None:
-        pageTitles.append("No title given")
-    else:
+    for result in results:
+        
+        link = result.find("a").get("href")
+        resultTitle = result.find("h3").getText()
+        resultDesc = result.find("span", class_="st").getText()
+
+        actualPage = getPage(link)
+        
+        pageTitleElement = actualPage.find("title")
+        pageTitle = "No title given" if pageTitleElement is None else pageTitleElement.getText()
+            
+        pageDescElement = actualPage.find("meta", attrs={"name": re.compile("[d|D]escription")})
+        pageDesc = "No description given" if pageDescElement is None else pageDescElement.get("content")
+
+        links.append(link)
+        resultTitles.append(resultTitle)
+        resultDescs.append(resultDesc)
         pageTitles.append(pageTitle)
-        
-    pageDesc = actualPage.find("meta", attrs={"name": re.compile("[d|D]escription")})
-    if pageDesc is None:
-        pageDescs.append("No description given")
-    else:
-        pageDescs.append(pageDesc.get("content"))
+        pageDescs.append(pageDesc)
+
+        print(f"{link}, {pageTitle}, {resultTitle}, {pageDesc}, {resultDesc}")
+
+resultsForPage(0)
+resultsForPage(10)
+resultsForPage(20)
 
 writeToCsv()
-
-
-print("Saved as " + fileName)
 
         
 
